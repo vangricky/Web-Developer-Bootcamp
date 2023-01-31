1288837bf1d3aaaa51d9f658e4d44ff5c4bbc7ee
Ricky Vang <vangcheemeng@yahoo.com>
2023-01-30 21:00:47 -0700

    completed the challenge by myself

---
+++ Javascript Challenge - Disappearing Circles.js
@@ -0,0 +1,11 @@
+document.getElementById("red-circle").onclick = function() {
+    document.getElementById("red-circle").style.display = "none";
+}
+
+document.getElementById("blue-circle").onclick = function() {
+    document.getElementById("blue-circle").style.display = "none";
+}
+
+document.getElementById("yellow-circle").onclick = function() {
+    document.getElementById("yellow-circle").style.backgroundColor = "green";
+}
